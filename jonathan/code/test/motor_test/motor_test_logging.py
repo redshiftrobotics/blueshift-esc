54d4e72695a7a746815332f2eb18985dc8b3e693
KaviMD <kaviasher@gmail.com>
2020-11-27 03:31:50 -0800

    Fix bugs in motor_test_logging

--- jonathan/code/test/motor_test/motor_test_logging.py
+++ jonathan/code/test/motor_test/motor_test_logging.py
@@ -1,22 +1,28 @@
 import serial
-import sys
+import argparse
 import time
 import csv
 import os
 
+parser = argparse.ArgumentParser(description='Gather motor data from an arduino')
+parser.add_argument('port', type=str, help='Arduino serial port')
+parser.add_argument('--save-dir', type=str, default="data", help="Arduino serial port (Default: 'data/')")
+
+args = parser.parse_args()
+
 # When running this program, specify the arduino port as a command line argument ex:
-# * python motortest.py COM5
+# * python motor_test_logging.py COM5
 # * Run `python -m serial.tools.list_ports` to list the available ports
 
 data_folder = "data"
 lines = []
 
-with serial.Serial(sys.argv[1], 9600, timeout=1) as ser:
+with serial.Serial(args.port, 9600, timeout=1) as ser:
     time.sleep(2)
 
     ser.write(b";STRT TST;")
     ser.flushInput()
-    print("Requested Test Initiation")
+    print("Sent Arduino Test Initiation Request")
 
     line = ""
     while line != ";STRT;":
@@ -42,11 +48,13 @@ for line in lines:
             tests[current_test] = [["speed", "amps", "volts"]]
         tests[current_test].append(parsed_line)
 
+
+data_path = os.path.join(data_folder, str(time.time()))
+if not os.path.exists(data_path):
+    os.makedirs(data_path)
+
 for test in tests:
-    data_path = os.path.join(data_folder, time.time())
-    if not os.path.exists(data_path):
-        os.makedirs(data_path)
-    with open(os.path.join(data_path, test+".csv"),"w+") as test_csv:
+    with open(os.path.join(data_path, test+".csv"), "w+", newline='') as test_csv:
         csvWriter = csv.writer(test_csv, delimiter=',')
         csvWriter.writerows(tests[test])
-print("Data Saved")
+print("Test Results Saved")
