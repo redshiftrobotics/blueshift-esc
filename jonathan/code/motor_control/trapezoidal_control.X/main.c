793a078d21405c93d5290aff776cc8b38667e823
Kavi Dey <kaviasher@gmail.com>
2021-04-29 22:48:07 -0700

    switch p channel mosfet on state

--- jonathan/code/motor_control/trapezoidal_control.X/main.c
+++ jonathan/code/motor_control/trapezoidal_control.X/main.c
@@ -29,13 +29,14 @@
 
 #include <xc.h>
 
-static int speed = 3000;
+static int speed = 4000;
 
 int step = 0;
 
 void __interrupt(no_auto_psv) _T1Interrupt(void) {
     LATAbits.LATA0 ^= 1;
     
+    /*
     step++;
     
     if (step > 5) {
@@ -45,59 +46,67 @@ void __interrupt(no_auto_psv) _T1Interrupt(void) {
     switch (step) {
         case 0:
             // A High; B Low; C Crossing
-            PDC1 = speed;
+            PDC1 = 0;
             SDC1 = 0;
-            PDC2 = 0;
+            PDC2 = speed;
             SDC2 = speed;
-            PDC4 = 0;
+            PDC4 = speed;
             SDC4 = 0;
             break;
         case 1:
             // A High; B Crossing; C Low
-            PDC1 = speed;
+            PDC1 = 0;
             SDC1 = 0;
-            PDC2 = 0;
+            PDC2 = speed;
             SDC2 = 0;
-            PDC4 = 0;
+            PDC4 = speed;
             SDC4 = speed;
             break;
         case 2:
             // A Crossing; B High; C Low
-            PDC1 = 0;
+            PDC1 = speed;
             SDC1 = 0;
-            PDC2 = speed;
+            PDC2 = 0;
             SDC2 = 0;
-            PDC4 = 0;
+            PDC4 = speed;
             SDC4 = speed;
             break;
         case 3:
             // A Low; B High; C Crossing
-            PDC1 = 0;
+            PDC1 = speed;
+            SDC1 = speed;
+            PDC2 = 0;
+            SDC2 = 0;
+            PDC4 = speed;
+            SDC4 = 0;
+            break;
+        case 4:
+            // A Low; B Crossing; C High
+            PDC1 = speed;
             SDC1 = speed;
             PDC2 = speed;
             SDC2 = 0;
             PDC4 = 0;
             SDC4 = 0;
             break;
-        case 4:
-            // A Low; B Crossing; C High
-            PDC1 = 0;
-            SDC1 = speed;
-            PDC2 = 0;
-            SDC2 = 0;
-            PDC4 = speed;
-            SDC4 = 0;
-            break;
         case 5:
             // A Crossing; B Low; C High
-            PDC1 = 0;
+            PDC1 = speed;
             SDC1 = 0;
-            PDC2 = 0;
+            PDC2 = speed;
             SDC2 = speed;
-            PDC4 = speed;
+            PDC4 = 0;
             SDC4 = 0;
             break;
     }
+    */
+    
+    PDC1 = 0;
+    SDC1 = 0;
+    PDC2 = 0;
+    SDC2 = 0;
+    PDC4 = 0;
+    SDC4 = 0;
     
     IFS0bits.T1IF = 0; // Reset the Timer1 interrupt
 }
