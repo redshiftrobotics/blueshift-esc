78c72c67665ab1b31984ed66d3540cd257802e96
AchillesHam <achillesham@seattleacademy.org>
2022-08-20 11:53:44 -0700

    Add comments to distinguish words from regular registers

--- code/motor_control/trapezoidal_control.X/i2c/ESC_registers.h
+++ code/motor_control/trapezoidal_control.X/i2c/ESC_registers.h
@@ -7,8 +7,10 @@ uint8_t PowerUp = 0x00;
 uint8_t LastCommandSuccess = 0x01;
 uint8_t BadCommand = 0x02;
 uint8_t ResetPic = 0x03;
+//Word
 uint8_t MotorSpeed0 = 0x04;
 uint8_t MotorSpeed1 = 0x05;
+//Word2 electric boogaloo
 uint8_t CurrentLimit0 = 0x06;
 uint8_t CurrentLimit1 = 0x07;
 
